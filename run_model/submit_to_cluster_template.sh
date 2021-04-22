c628e8fbb1f3df0b317a19d45771bab795ed1e21
dlavrent <dlvrntvch@gmail.com>
2021-04-22 02:26:02 -0400

    Create submit_to_cluster_template.sh

---
+++ run_model/submit_to_cluster_template.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -t 0-04:20
+#SBATCH -p serial_requeue
+#SBATCH --account=debivort_lab
+#SBATCH --mem=64000
+#SBATCH --mail-user=dlavrent@g.harvard.edu
+#SBATCH --mail-type=ALL
+
+module load Anaconda3/5.0.1-fasrc02
+source activate py3p6
+python3 run_sim.py
