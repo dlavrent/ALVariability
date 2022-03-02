eeb083ad2745a3c3b2030d27ff8133399cb1dd1a
dlavrent <dlvrntvch@gmail.com>
2022-03-02 17:03:28 -0500

    add user hemibrain access token

---
+++ connectomics/hemibrain_access_token.py
@@ -0,0 +1,7 @@
+# utility file for storing an access token for the hemibrain Python API
+# first, make an account on https://neuprint.janelia.org/
+# then, go to your account tab (https://neuprint.janelia.org/account), 
+# find your "Auth Token", and paste it below.
+# this is used in connectomics/get_AL_neurons_v1.2.ipynb
+
+my_token = 'your token here'
